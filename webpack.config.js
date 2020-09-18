b78e7d28c00d979b7a16db5591cb3aa814989c9f
Onur Cem <onurcemsenel@gmail.com>
2020-09-19 00:03:23 +0300

    chore: add TypeScript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
 const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const JS_JSX_PATTERN = /\.jsx?$/i;
+const JS_JSX_TS_TSX_PATTERN = /\.(js|ts)x?$/i;
 const SASS_PATTERN = /\.s[ac]ss$/i;
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: [
-    './src/polyfills.js',
-    './src/App.jsx'
-  ],
+  entry: ['./src/polyfills.ts', './src/App.tsx'],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: JS_JSX_PATTERN,
+        test: JS_JSX_TS_TSX_PATTERN,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: SASS_PATTERN,
@@ -29,33 +27,35 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              hmr: isDev
-            }
+              hmr: isDev,
+            },
           },
           'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
     }),
-    new MiniCssExtractPlugin()
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
   ],
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
-    modules: [
-      path.resolve(__dirname, 'src'),
-      'node_modules'
-    ]
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
   devServer: {
     port: 3000,
-    hot: true
-  }
+    hot: true,
+  },
 };
