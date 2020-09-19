9a780443e7172499bbcbfb1fd62f52397c313f6b
Onur Cem <onurcemsenel@gmail.com>
2020-09-20 02:51:23 +0300

    chore: define redux-thunk related types

--- src/common/rootReducer.ts
+++ src/common/rootReducer.ts
@@ -1,9 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import commonReducer from 'common/commonSlice';
+import dashboardReducer from 'modules/dashboard/dashboardSlice';
 
 const rootReducer = combineReducers({
   common: commonReducer,
+  dashboard: dashboardReducer,
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
--- src/common/store.ts
+++ src/common/store.ts
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 
-import rootReducer from 'common/rootReducer';
+import rootReducer, { RootState } from 'common/rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
 });
+
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
 export default store;
